3f38df973f3dd0c868801953525da1e2067bed18
nish <249183418@qq.com>
2017-10-17 15:24:32 +0800

    up

--- Blog/Home/Controller/TestController.class.php
+++ Blog/Home/Controller/TestController.class.php
@@ -10,10 +10,10 @@ class TestController extends Controller {
     	// import('Think.Cache.Driver.Redis'); 
     	// $redis = new \Think\Cache\Driver\Redis();
     	// $redis = new \Think\Cache();
-    	// $redis = new \Redis();    
-		// $redis->connect('127.0.0.1',6379);
-		// $redis->set('test','hello world!');
-		// echo $redis->get("test");
+    	$redis = new \Redis();    
+		$redis->connect('127.0.0.1',6379);
+		$redis->set('test','hello world!');
+		echo $redis->get("test");
 
 		
 	    S('test','1234');
